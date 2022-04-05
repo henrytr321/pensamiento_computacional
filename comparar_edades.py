97ab8442f126812a885358645e03b256c58c3915
henrytr321 <henrytr321@gmail.com>
2022-04-04 20:57:41 -0500

    Se agrega el primer reto para averiguar quien tiene más edad entre dos personas

---
+++ comparar_edades.py
@@ -0,0 +1,9 @@
+edad_1 = int(input('Ingresa tu edad: '))
+edad_2 = int(input('Ingresa la edad de tu amigo: '))
+
+if edad_1 > edad_2:
+    print('La primera persona tiene más edad')
+elif edad_1 > edad_2:
+    print('La segunda persona tiene más edad')
+else:
+    print('Los dos tienen la misma edad')
