c9a3004f03e1addf9b4b815a4a502692fbf631e7
henrytr321 <henrytr321@gmail.com>
2022-04-05 21:46:23 -0500

    Finalizadas pruebas de caja negra

--- caja_negra.py
+++ caja_negra.py
@@ -1,7 +1,9 @@
 import unittest
 
 def suma(num_1, num_2):
-    return num_1 + num_2
+    return abs(num_1) + num_2
+
+
 
 class CajaNegraTest(unittest.TestCase):
 
@@ -12,6 +14,14 @@ class CajaNegraTest(unittest.TestCase):
         resultado = suma(num_1, num_2)
 
         self.assertEqual(resultado, 15)
+
+    def test_suma_dos_negativos(self):
+        num_1 = -10
+        num_2 = -7
+
+        resultado = suma(num_1, num_2)
+
+        self.assertEqual(resultado, -17)
 
 if __name__ == '__main__':
     unittest.main()
